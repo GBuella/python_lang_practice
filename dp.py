7db406338b4264f7ae654c322a095a07dc596747
Gabor Buella <gbuella@gmail.com>
2025-06-18 22:31:34 +0200

    Add some prepositions

--- dp.py
+++ dp.py
@@ -36,8 +36,10 @@ startid = 0
 endid = 0
 
 parser = argparse.ArgumentParser(description='Noun declensions.')
-parser.add_argument('-p', help='CSV file', default='pldb.csv',
+parser.add_argument('-p', help='CSV file - nouns', default='pldb.csv',
 		dest='path')
+parser.add_argument('-P', help='CSV file - prepositions', default='pldb_prep.csv',
+		dest='prepdb_path')
 parser.add_argument('-g', help='gender filter', default='all',
 		dest='gender')
 parser.add_argument('-c', help='case filter', default='all',
@@ -48,14 +50,46 @@ parser.add_argument('-s', help='start id', default=0, type=int,
 		dest='startid')
 parser.add_argument('-e', help='end id', default=0, type=int,
 		dest='endid')
-parser.add_argument('-f', help='end id', default=0, type=bool,
+parser.add_argument('-f', help='end id', action='store_true',
 		dest='full6')
-parser.add_argument('-F', help='end id', default=0, type=bool,
+parser.add_argument('-F', help='end id', action='store_true',
 		dest='full7')
+parser.add_argument('-C', help='ask compounds', action='store_true',
+		dest='preps')
+parser.add_argument('-r', help='randomize case order', action='store_true',
+		dest='rcases')
 
 args = parser.parse_args()
 
 words = []
+preps = {}
+
+if args.preps:
+	with open(args.prepdb_path, newline='') as csvfile:
+		datareader = csv.reader(csvfile, delimiter='\t', quotechar='|')
+		for row in datareader:
+			if row[0].startswith('#'):
+				continue
+			item = {}
+			item['preposition'] = row[0]
+			case = row[1]
+			if row[2] != '':
+				item['question_prep'] = row[2]
+			if len(row) > 3 and row[3] != '':
+				item['question_postp'] = row[3]
+			if len(row) > 4 and row[4] != '':
+				item['number'] = row[4]
+			if 'number' in item:
+				if not (case + '_' + item['number']) in preps:
+					preps[case + '_' + item['number']] = []
+				preps[case + '_' + item['number']].append(item)
+			else:
+				if not (case + '_singular') in preps:
+					preps[case + '_singular'] = []
+				preps[case + '_singular'].append(item)
+				if not (case + '_plural') in preps:
+					preps[case + '_plural'] = []
+				preps[case + '_plural'].append(item)
 
 with open(args.path, newline='') as csvfile:
 	datareader = csv.reader(csvfile, delimiter='\t', quotechar='|')
@@ -108,19 +142,42 @@ with open(args.path, newline='') as csvfile:
 
 random.shuffle(words)
 
-def ask(form, answer, ns, prev):
-	resp = input(form + ': ')
+def ask(case, number, answer, ns, prev):
+	answer2 = answer
+	prompt = case + '   ' + number
+	answer_prefix = ''
+	if args.preps and (case + '_' + number) in preps:
+		prep = random.choice(preps[case + '_' + number])
+		if 'question_prep' in prep:
+			prompt = prep['question_prep'] + '   '
+		else:
+			prompt = ''
+		prompt = prompt + number
+		if 'question_postp' in prep:
+			prompt = prompt + '   ' + prep['question_postp']
+		answer = prep['preposition'] + ' ' + answer
+		# TODO explicit rules for ze, we, pode...
+		if (prep['preposition'].endswith('z')
+			or prep['preposition'].endswith('d')
+			or prep['preposition'].endswith('w')):
+			answer2 = prep['preposition'] + 'e ' + answer
+		else:
+			answer2 = answer
+	prompt = prompt + ':  '
+	prompt = prompt.rjust(50)
+
+	resp = ' '.join(input(prompt).split()).strip()
 	if resp == 'x':
 		resp = prev
 	c = 0
-	while (resp != answer):
+	while (resp != answer and resp != answer):
 		if resp == '':
 			c += 1
 			if c == 1:
 				print('The word is ' + ns)
 			else:
 				print('It is "' + answer + '"')
-		resp = input(form + ': ')
+		resp = input(prompt)
 
 for picked in words:
 	if (args.number == 'singular' and (
@@ -140,6 +197,8 @@ for picked in words:
 	assert picked['decl'][0][0] == 'nominative'
 	ns = picked['decl'][0][1]
 	prev = ''
+	if args.rcases:
+		random.shuffle(picked['decl'])
 	for d in picked['decl']:
 		if args.case == 'all' and ((not args.full7) and d[0] == 'vocative'):
 			continue;
@@ -148,7 +207,7 @@ for picked in words:
 		if (args.number != 'plural'
 			and picked['gender'] != 'viril_plural'
 			and picked['gender'] != 'nonviril_plural'):
-			ask(d[0] + ' singular', d[1], ns, prev)
+			ask(d[0], 'singular', d[1], ns, prev)
 			prev = d[1]
 		if (args.number != 'singular'
 			and (args.full6 or args.full7 or picked['irregular'] or
@@ -161,5 +220,5 @@ for picked in words:
 				if (len(d) == 2):
 					continue
 				answer = d[2]
-			ask(d[0] + ' plural', answer, ns, prev)
+			ask(d[0], 'plural', answer, ns, prev)
 			prev = answer
